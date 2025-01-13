76a10e466ca100bc2b289eab9eb84b463b851192
peter varga <pkvarg@yahoo.se>
2025-01-13 18:02:25 +0100

    audio fixed

--- frontend/src/screens/LifeStudy.jsx
+++ frontend/src/screens/LifeStudy.jsx
@@ -1,45 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { lifeStudy } from '../functions'
+import axios from 'axios'
 
 const LifeStudy = () => {
-  const [subcategory, setSubcategory] = useState('Studium života')
+  const [lifeStudy, setLifeStudy] = useState([])
 
   const category = 'Studium života'
 
+  useEffect(() => {
+    const getAudio = async () => {
+      const { data } = await axios.get(`/api/audio`)
+      const audios = data.audios
+      const lsAudios = audios.filter((audio) => audio.category === category)
+      setLifeStudy(lsAudios)
+    }
+    getAudio()
+  }, [])
+
   return (
-    <div className='margined'>
-      <Link className='btn btn-back my-3' to='/'>
+    <div className="margined">
+      <Link className="btn btn-back my-3" to="/">
         Zpět
       </Link>
 
-      <div className='give2-physical'>
-        <h1 className='my-3'>Posluchárna</h1>
+      <div className="give2-physical">
+        <h1 className="my-3">Posluchárna</h1>
 
-        <h3 className='my-3'>STUDIUM ŽIVOTA</h3>
+        <h3 className="my-3">STUDIUM ŽIVOTA</h3>
         <p>
-          Dvacetiminutové relace jsou věnovány rozsáhlému dílu Witnesse Leeho –
-          Studiu života v Bibli. Studium života v Bibli je obsáhlé a klasické
-          knižní dílo, které navazuje na vše, co Pán zjevil své církvi v průběhu
-          minulých staletí až do dnešních dnů. Se svými více než 25 000 stranami
-          komentářů ke všem biblickým knihám se řadí k nejbohatším současným a
+          Dvacetiminutové relace jsou věnovány rozsáhlému dílu Witnesse Leeho – Studiu života v
+          Bibli. Studium života v Bibli je obsáhlé a klasické knižní dílo, které navazuje na vše, co
+          Pán zjevil své církvi v průběhu minulých staletí až do dnešních dnů. Se svými více než 25
+          000 stranami komentářů ke všem biblickým knihám se řadí k nejbohatším současným a
           aktuálním výkladům biblické pravdy.
         </p>
       </div>
-      <div className='iframes-grid'>
-        {lifeStudy.map((url) => (
-          <div key={url._id} className='iframe-w life-study-top'>
-            <div className='mp3-frame-desc'>
-              <p className='sub'>{url.subcategory}</p>
-              <p className='tit'>{url.audioTitle}</p>
-              <p className='download'>Stáhnout</p>
+      <div className="iframes-grid">
+        {lifeStudy?.map((url) => (
+          <div key={url._id} className="iframe-w life-study-top">
+            <div className="mp3-frame-desc">
+              <p className="sub">{url.subcategory}</p>
+              <p className="tit">{url.audioTitle}</p>
+              <p className="download">Stáhnout</p>
             </div>
-            <iframe
-              src={url.mp3file}
-              width='640'
-              height='480'
-              allow='autoplay'
-            ></iframe>
+            <iframe src={url.mp3file} width="640" height="480" allow="autoplay"></iframe>
           </div>
         ))}
       </div>
--- frontend/src/screens/WordsOfLife.jsx
+++ frontend/src/screens/WordsOfLife.jsx
@@ -1,258 +1,177 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useRef } from 'react'
-import { wordsOflife } from '../functions'
+import axios from 'axios'
 
 const WordsOfLife = () => {
   const [subcategory, setSubcategory] = useState('Boží evangelium')
+  const [wordsOfLife, setWordsOfLife] = useState([])
   const myRef = useRef(null)
   const subHandler = (sub) => {
     myRef.current.scrollIntoView({ behavior: 'smooth' })
     setSubcategory(sub)
   }
 
-  const category = 'SLOVA ŽIVOTA A PRAVDY'
+  const category = 'Slova života'
+
+  useEffect(() => {
+    const getAudio = async () => {
+      const { data } = await axios.get(`/api/audio`)
+      const audios = data.audios
+      const wolAudios = audios.filter((audio) => audio.category === category)
+      setWordsOfLife(wolAudios)
+    }
+    getAudio()
+  }, [])
 
   return (
     <>
-      <Link className='btn btn-back my-3' to='/'>
+      <Link className="btn btn-back my-3" to="/">
         Zpět
       </Link>
 
-      <div className='podcast'>
-        <h1 className='my-3'>Posluchárna</h1>
-        <h3 className='my-3'>SLOVA ŽIVOTA A PRAVDY</h3>
+      <div className="podcast">
+        <h1 className="my-3">Posluchárna</h1>
+        <h3 className="my-3">SLOVA ŽIVOTA A PRAVDY</h3>
         <p>
-          Pořad Slova života a pravdy, který odvysílalo Rádio 7, je založen na
-          krátkých úryvcích z knih Watchmana Neeho a Witnesse Leeho. Jednotlivé,
-          zhruba patnáctiminutové nahrávky přinášejí svěží pohled na pravdu
-          zjevenou v Písmu z perspektivy božského života, z něhož se těší
-          všichni věřící v Krista.{' '}
+          Pořad Slova života a pravdy, který odvysílalo Rádio 7, je založen na krátkých úryvcích z
+          knih Watchmana Neeho a Witnesse Leeho. Jednotlivé, zhruba patnáctiminutové nahrávky
+          přinášejí svěží pohled na pravdu zjevenou v Písmu z perspektivy božského života, z něhož
+          se těší všichni věřící v Krista.{' '}
         </p>
       </div>
-      <div className='subcategories'>
-        <div className='subcategories-frame no-mobile'>Předmět</div>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Bůh v listu Římanům')}
-        >
+      <div className="subcategories">
+        <div className="subcategories-frame no-mobile">Předmět</div>
+        <button className="btn-subcategory" onClick={() => subHandler('Bůh v listu Římanům')}>
           Bůh v listu Římanům
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Boží evangelium')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Boží evangelium')}>
           Boží evangelium
         </button>{' '}
         <button
-          className='btn-subcategory'
+          className="btn-subcategory"
           onClick={() => subHandler('Charakter Pánova pracovníka')}
         >
           Charakter Pánova pracovníka
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Člověk a dva stromy')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Člověk a dva stromy')}>
           Člověk a dva stromy
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Evangelium království')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Evangelium království')}>
           Evangelium království
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Fakt, víra a prožitek')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Fakt, víra a prožitek')}>
           Fakt, víra a prožitek
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Hlavní Kristovy kroky')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Hlavní Kristovy kroky')}>
           Hlavní Kristovy kroky
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Kristovo vzkříšení')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Kristovo vzkříšení')}>
           Kristovo vzkříšení
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Kristus jako slitovnice')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Kristus jako slitovnice')}>
           Kristus jako slitovnice
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Křesťanský život')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Křesťanský život')}>
           Křesťanský život
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Milostivé léto')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Milostivé léto')}>
           Milostivé léto
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Naplnění Starého zákona')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Naplnění Starého zákona')}>
           Naplnění Starého zákona
         </button>{' '}
         <button
-          className='btn-subcategory'
+          className="btn-subcategory"
           onClick={() => subHandler('Nevystižitelné Kristovo bohatství')}
         >
           Nevystižitelné Kristovo bohatství
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('O člověku')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('O člověku')}>
           O člověku
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('O Duchu')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('O Duchu')}>
           O Duchu
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('O Kristovi')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('O Kristovi')}>
           O Kristu
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Poselství evangelia')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Poselství evangelia')}>
           Poselství evangelia
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Prožívání Krista')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Prožívání Krista')}>
           Prožívání Krista
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Rozdílení života')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Rozdílení života')}>
           Rozdílení života
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Řada pro nové věřící')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Řada pro nové věřící')}>
           Řada pro nové věřící
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Spasení')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Spasení')}>
           Spasení
         </button>
         <button
-          className='btn-subcategory'
+          className="btn-subcategory"
           onClick={() => subHandler('Struktura Božího evangelia')}
         >
           Struktura Božího evangelia
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Svědomí')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Svědomí')}>
           Svědomí
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Trojnásobné semeno')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Trojnásobné semeno')}>
           Trojnásobné semeno
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Učení apoštolů')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Učení apoštolů')}>
           Učení apoštolů
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Věčný Boží plán')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Věčný Boží plán')}>
           Věčný Boží plán
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Vypořádání se s hříchy')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Vypořádání se s hříchy')}>
           Vypořádání se s hříchy
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Vypořádání se se světem')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Vypořádání se se světem')}>
           Vypořádání se se světem
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Vzepřít se Satanovi')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Vzepřít se Satanovi')}>
           Vzepřít se Satanovi
         </button>
         <button
-          className='btn-subcategory'
+          className="btn-subcategory"
           onClick={() => subHandler('Základní prvky křesťanského života')}
         >
           Základní prvky křesťanského života
         </button>
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Zjevení života')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Zjevení života')}>
           Zjevení života
         </button>
         <button
-          className='btn-subcategory'
-          onClick={() =>
-            subHandler('Zkušenosti věřících s Kristovým vzkříšením')
-          }
+          className="btn-subcategory"
+          onClick={() => subHandler('Zkušenosti věřících s Kristovým vzkříšením')}
         >
           Zkušenosti věřících s Kristovým vzkříšením
         </button>{' '}
-        <button
-          className='btn-subcategory'
-          onClick={() => subHandler('Zkušenost života')}
-        >
+        <button className="btn-subcategory" onClick={() => subHandler('Zkušenost života')}>
           Zkušenost života
         </button>{' '}
       </div>
 
       <div ref={myRef}></div>
-      <div className='iframes-grid'>
-        {wordsOflife.map(
+      <div className="iframes-grid">
+        {wordsOfLife?.map(
           (url) =>
             url.subcategory === subcategory && (
-              <div key={url._id} className='iframe-w'>
-                <div className='mp3-frame-desc'>
-                  <p className='sub'>{url.subcategory}</p>
-                  <p className='tit'>{url.audioTitle}</p>
-                  <p className='download'>Stáhnout</p>
+              <div key={url._id} className="iframe-w">
+                <div className="mp3-frame-desc">
+                  <p className="sub">{url.subcategory}</p>
+                  <p className="tit">{url.audioTitle}</p>
+                  <p className="download">Stáhnout</p>
                 </div>
-                <iframe
-                  src={url.mp3file}
-                  width='640'
-                  height='480'
-                  allow='autoplay'
-                ></iframe>
+                <iframe src={url.mp3file} width="640" height="480" allow="autoplay"></iframe>
               </div>
-            )
+            ),
         )}
       </div>
     </>
