9fb5a69f6c107f099b30bbd9d240e648faccdc0b
peter varga <pkvarg@yahoo.se>
2025-01-13 20:53:41 +0100

    Footer back

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { firebaseInitApp } from '../App'
 import { getAnalytics } from 'firebase/analytics'
 import axios from 'axios'
+import { Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const CookieConsentModal = () => {
   const [isModalVisible, setIsModalVisible] = useState(false)
@@ -71,6 +73,111 @@ const CookieConsentModal = () => {
           </div>
         </div>
       )}
+      <footer>
+        <Container>
+          <div className="footer-text">
+            <Row>
+              <Col>
+                <div className="footer-text-links ">
+                  <h2>Informace</h2>
+                  <Link to="/about">
+                    <p>O nás</p>
+                  </Link>
+                  <Link to="/contact">
+                    <p className="footer-trade-rules">Kontaktujte nás</p>
+                  </Link>
+                </div>
+              </Col>
+              <Col>
+                <div className="footer-text-links ">
+                  <h2>Podmínky</h2>
+                  <Link to="/safety-privacy">
+                    <p>GDPR</p>
+                  </Link>
+                  <Link to="/trade-rules">
+                    <p className="footer-trade-rules">Obchodní podmínky</p>
+                  </Link>
+                </div>
+              </Col>
+              <Col>
+                <div className="footer-text-links ">
+                  <h2>Váš účet</h2>
+                  <Link to="/login?redirect=/profile">
+                    <p>Objednávky</p>
+                  </Link>
+                  <Link to="/forgot-password">
+                    <p className="footer-trade-rules">Zapomenuté heslo</p>
+                  </Link>
+                </div>
+              </Col>
+              <Col>
+                <div className="footer-text-links ">
+                  <h2>Kontakt</h2>
+
+                  <a href="mailto:admin@prud.sk">
+                    <p>proud@proudnihy.cz</p>
+                  </a>
+
+                  <a href="tel:+421904060262">
+                    <p>+420 724 526 926</p>
+                  </a>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+        <Container>
+          <Row>
+            <Col className="py-3">
+              <div className="footer-copyright no-mobile">
+                <p className="footer-link">
+                  Copyright &copy; {Date().substring(11, 15)} Distribuce Proud, všechna práva
+                  vyhrazena,
+                </p>
+                <a
+                  href="https://www.lsm.org"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="footer-link"
+                  style={{ marginLeft: '2px' }}
+                >
+                  se svolením Living Stream Ministry
+                </a>
+                <a
+                  href="https://www.pictusweb.sk"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="footer-link"
+                >
+                  &#60;&#47;&#62; PICTUSWEB Development
+                </a>
+              </div>
+
+              <div className="footer-copyright mobile-only">
+                <p>Copyright &copy; {Date().substring(11, 15)} Distribuce Proud,</p>
+                <p>všechna práva vyhrazena, se svolením</p>
+                {/* <p> s povolením</p> */}
+                <a
+                  href="https://www.lsm.org"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="footer-link"
+                >
+                  LIVING STREAM MINISTRY
+                </a>
+                <a
+                  href="https://www.pictusweb.sk"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="footer-link"
+                >
+                  &#60;&#47;&#62; PICTUSWEB Development
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </div>
   )
 }
